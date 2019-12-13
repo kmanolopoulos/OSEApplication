c460c1d298b5f797b7295a6073e5dcfe9d817b68
kmanolopoulos <kmanolop@otenet.gr>
2019-12-14 01:18:56 +0200

    - Added a synchronize choice that will get timesheet data from internet

--- app/src/main/java/com/kmanolopoulos/oseapplication/MainActivity.kt
+++ app/src/main/java/com/kmanolopoulos/oseapplication/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.kmanolopoulos.oseapplication
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.View
 import android.widget.Button
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.view.*
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,18 +16,22 @@ class MainActivity : AppCompatActivity() {
     override fun onPostCreate(savedInstanceState: Bundle?) {
         super.onPostCreate(savedInstanceState)
 
-        findViewById<Button>(R.id.btn_main_search).setOnClickListener() { onChoice(it) }
-        findViewById<Button>(R.id.btn_main_favourite).setOnClickListener() { onChoice(it) }
-        findViewById<Button>(R.id.btn_main_about).setOnClickListener() { onChoice(it) }
+        findViewById<Button>(R.id.btn_main_search).setOnClickListener { onChoice(it) }
+        findViewById<Button>(R.id.btn_main_synchronize).setOnClickListener { onChoice(it) }
+        findViewById<Button>(R.id.btn_main_favourite).setOnClickListener { onChoice(it) }
+        findViewById<Button>(R.id.btn_main_about).setOnClickListener { onChoice(it) }
     }
 
     fun onChoice(view: View) {
-        var intent : Intent
+        var intent: Intent
 
         when (view.id) {
             R.id.btn_main_search -> {
                 intent = Intent(this, SearchActivity::class.java)
                 startActivity(intent)
+            }
+            R.id.btn_main_synchronize -> {
+
             }
             R.id.btn_main_favourite -> {
 
